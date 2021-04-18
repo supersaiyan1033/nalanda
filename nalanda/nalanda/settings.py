63a34e6e5d086b6d02f76381685d37673ca9dcc9
revanththota <cse190001063@iiti.ac.in>
2021-04-18 17:47:28 +0530

    backend for authentication(not yet completed)
    added apps,modified database credentals mailing system and aded backend for all authentication

--- nalanda/nalanda/settings.py
+++ nalanda/nalanda/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'nalanda',
+    'authentication',
+    'library',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -77,17 +80,19 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'nalanda',
-        'USER': 'root',
+        'USER': 'project',
         'PASSWORD': 'roomno605',
         'HOST': 'localhost',
-        'PORT': '3333',
+        'PORT': '3306',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
         }
 
     }
 }
-
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -127,7 +132,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST_USER='nalanda3306@gmail.com'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS =True
+EMAIL_HOST_PASSWORD='roomno@605'
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
